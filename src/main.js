189fef2fc977cf7b26bf4a81c44bb202707a2996
GamingInfinite <ender2gamer123@gmail.com>
2021-07-06 16:08:44 -0500

    Reading Settings
    I needed this before the refactor and now I'm happy

--- src/main.js
+++ src/main.js
@@ -2,31 +2,59 @@
 exports.__esModule = true;
 require("update-electron-app")();
 var electron_1 = require("electron");
+var fs = require("fs");
+var path = require("path");
 var win;
 var settings;
+var settingsJson = {
+    directory: "unset",
+    instances: {}
+};
 function createWindow() {
     win = new electron_1.BrowserWindow({
         width: 1200,
         height: 800,
         show: false
     });
-    settings = new electron_1.BrowserWindow({
-        parent: win,
-        modal: true,
-        show: false
-    });
+    // settings = new BrowserWindow({
+    //   parent: win,
+    //   modal: true,
+    //   show: false,
+    // });
     win.loadFile("./src/index.html");
-    settings.loadFile("./src/settings/settings.html");
+    // settings.loadFile("./src/settings/settings.html");
     win.once("ready-to-show", function () {
         win.show();
     });
-    settings.once("ready-to-show", function () {
-        settings.show();
-    });
+    // settings.once("ready-to-show", () => {
+    //   settings.show();
+    // });
+}
+function readSettings() {
+    var rawSettingsJson = fs.readFileSync(path.join(electron_1.app.getPath("userData"), "settings.json"));
+    settingsJson = JSON.parse(rawSettingsJson.toString());
+    console.log(settingsJson);
+}
+function writeSettings() {
+    var data = JSON.stringify(settingsJson);
+    fs.writeFileSync(path.join(electron_1.app.getPath("userData"), "settings.json"), data);
 }
 electron_1.app.whenReady().then(function () {
     createWindow();
     console.log(electron_1.app.getPath("userData"));
+    try {
+        if (fs.existsSync(path.join(electron_1.app.getPath("userData"), "settings.json"))) {
+            console.log("Proceeding Normally");
+        }
+        else {
+            console.log("Creating Initial Settings");
+            writeSettings();
+        }
+    }
+    catch (error) {
+        console.log("Executing Order 66...");
+    }
+    readSettings();
 });
 electron_1.app.on("window-all-closed", function () {
     if (process.platform !== "darwin")
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 require("update-electron-app")();
 import { app, BrowserWindow } from "electron";
+import * as fs from "fs";
+import * as path from "path";
 
 var win: BrowserWindow;
 var settings: BrowserWindow;
+
+var settingsJson = {
+  directory: "unset",
+  instances: {},
+};
 
 function createWindow() {
   win = new BrowserWindow({
@@ -11,27 +18,51 @@ function createWindow() {
     show: false,
   });
 
-  settings = new BrowserWindow({
-    parent: win,
-    modal: true,
-    show: false,
-  });
+  // settings = new BrowserWindow({
+  //   parent: win,
+  //   modal: true,
+  //   show: false,
+  // });
 
   win.loadFile("./src/index.html");
-  settings.loadFile("./src/settings/settings.html");
+  // settings.loadFile("./src/settings/settings.html");
 
   win.once("ready-to-show", () => {
     win.show();
   });
 
-  settings.once("ready-to-show", () => {
-    settings.show();
-  });
+  // settings.once("ready-to-show", () => {
+  //   settings.show();
+  // });
+}
+
+function readSettings() {
+  var rawSettingsJson = fs.readFileSync(
+    path.join(app.getPath("userData"), "settings.json")
+  );
+  settingsJson = JSON.parse(rawSettingsJson.toString());
+  console.log(settingsJson);
+}
+
+function writeSettings() {
+  var data = JSON.stringify(settingsJson);
+  fs.writeFileSync(path.join(app.getPath("userData"), "settings.json"), data);
 }
 
 app.whenReady().then(() => {
   createWindow();
   console.log(app.getPath("userData"));
+  try {
+    if (fs.existsSync(path.join(app.getPath("userData"), "settings.json"))) {
+      console.log("Proceeding Normally");
+    } else {
+      console.log("Creating Initial Settings");
+      writeSettings();
+    }
+  } catch (error) {
+    console.log("Executing Order 66...");
+  }
+  readSettings();
 });
 
 app.on("window-all-closed", function () {
