feaa7e8ffee020622873fefe0847ce79f23ebc38
GamingInfinite <ender2gamer123@gmail.com>
2021-07-06 14:01:20 -0500

    More Future Proofing
    Idk what to put here I'm just making sure I can do things

--- src/main.js
+++ src/main.js
@@ -2,19 +2,31 @@
 exports.__esModule = true;
 require("update-electron-app")();
 var electron_1 = require("electron");
+var win;
+var settings;
 function createWindow() {
-    var win = new electron_1.BrowserWindow({
+    win = new electron_1.BrowserWindow({
         width: 1200,
         height: 800,
         show: false
     });
+    settings = new electron_1.BrowserWindow({
+        parent: win,
+        modal: true,
+        show: false
+    });
     win.loadFile("./src/index.html");
+    settings.loadFile("./src/settings/settings.html");
     win.once("ready-to-show", function () {
         win.show();
+    });
+    settings.once("ready-to-show", function () {
+        settings.show();
     });
 }
 electron_1.app.whenReady().then(function () {
     createWindow();
+    console.log(electron_1.app.getPath("userData"));
 });
 electron_1.app.on("window-all-closed", function () {
     if (process.platform !== "darwin")
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,37 @@
 require("update-electron-app")();
 import { app, BrowserWindow } from "electron";
 
+var win: BrowserWindow;
+var settings: BrowserWindow;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1200,
     height: 800,
     show: false,
   });
 
+  settings = new BrowserWindow({
+    parent: win,
+    modal: true,
+    show: false,
+  });
+
   win.loadFile("./src/index.html");
+  settings.loadFile("./src/settings/settings.html");
 
   win.once("ready-to-show", () => {
     win.show();
+  });
+
+  settings.once("ready-to-show", () => {
+    settings.show();
   });
 }
 
 app.whenReady().then(() => {
   createWindow();
+  console.log(app.getPath("userData"));
 });
 
 app.on("window-all-closed", function () {
